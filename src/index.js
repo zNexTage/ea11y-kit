205c041b24ebb65b5d1008c396e5be2bfdceed7f
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-03-22 15:53:59 -0300

    Configuração inicial do projeto.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import Textbox from './lib/fields/Textbox';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Textbox label='Nome' id='txtName' type='text' />
   </React.StrictMode>
 );
 
