179a913a9dbd7a5ad10d3ec6c2ed03b1567ea6d3
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-03-22 18:27:39 -0300

    Criado constantes para as diretrizes do eMAG.

--- src/exceptions/GuidelineViolation/GuidelineViolation.js
+++ src/exceptions/GuidelineViolation/GuidelineViolation.js
@@ -1,6 +1,8 @@
+import guidelineMessages from "../../utils/eMagGuidelineMessage";
+
 class GuidelineViolation extends Error {
-    constructor(guidelineCode, guidelineMessage, message){
-       super(`Violação da diretriz ${guidelineCode} -  ${guidelineMessage} - ${message}`);
+    constructor(guidelineCode, message){        
+       super(`Violação da diretriz ${guidelineCode} - ${guidelineMessages.get(guidelineCode)}: ${message} Acesse a ferramenta de desenvolvimento do navegador pressionando F12 para verificar mais detalhes.`);
     }
 }
 
---
+++ src/utils/eMagGuidelineMessage.js
@@ -0,0 +1,8 @@
+import { ASSOCIATE_TAGS_WITH_YOUR_FIELDS, PROVIDE_INSTRUCTIONS_FOR_DATA_ENTRY } from "./eMagGuidelineCode";
+
+const guidelineMessages = new Map();
+
+guidelineMessages.set(ASSOCIATE_TAGS_WITH_YOUR_FIELDS, "Associar etiquetas aos seus campos");
+guidelineMessages.set(PROVIDE_INSTRUCTIONS_FOR_DATA_ENTRY, "Fornecer instruções para entrada de dados");
+
+export default guidelineMessages;
