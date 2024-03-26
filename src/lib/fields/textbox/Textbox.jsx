7f667143199e7ef11cc00596c49ad464aaeae3e1
christian <christianoliveira1908@hotmail.com>
2024-03-26 11:07:00 -0300

    Documentação: Textbox

--- src/lib/fields/textbox/Textbox.jsx
+++ src/lib/fields/textbox/Textbox.jsx
@@ -3,7 +3,7 @@ import GuidelineViolation from "../../../exceptions/GuidelineViolation/Guideline
 import style from "./Textbox.module.css";
 import { PROVIDE_INSTRUCTIONS_FOR_DATA_ENTRY } from "../../../utils/eMagGuidelineCode";
 import useFieldValidations from "../../hooks/validations/useFieldValidations";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import RequiredAttribute from "../../../exceptions/RequiredAttribute";
 import ComponentErrorList from "../../../components/component-error-list";
 
@@ -21,15 +21,29 @@ const AVAILABLE_TYPES = ["email", "number", "password", "search", "text", "url"]
 
 /** 
  * Campo de texto configurado com as diretrizes do eMAG. 
+ * 
+ * Diretrizes adotadas:
+ * 
+ * Recomendação 4.4 – Possibilitar que o elemento com foco seja visualmente evidente
+ * - Ao receber foco é aplicado uma borda vermelha de 2px do tipo solid no campo de texto.
+ * Além da borda, foi reforçado o destaque do componente através do atributo outline.
+ * 
+ * 
  * Recomendação 6.2 – Associar etiquetas aos seus campos: 
  *  - O atributo id é obrigatório, e é utilizado para vincular a label ao campo de texto e identificar o input;
+ * 
  * Recomendação 6.5 – Fornecer instruções para entrada de dados: 
- *  - Para os campos obrigatórios, é adicionado a informação *campo obrigatório* a frente da label para que
+ *  - Para os campos obrigatórios é adicionado a informação *campo obrigatório* a frente da label para que
  * leitores de telas possam comunicar ao usuário que o campo precisa ser preenchido;
  *  - O atributo *placeholder* é obrigatório para os campos e deverá apresentar uma dica de preenchimento do campo.
- * A dica é importante para os leitores de tela.
+ * A dica é importante para os leitores de tela. 
+ * 
+ * O componente é renderizado apenas se estiver de acordo com as diretrizes do eMAG. Caso não esteja,
+ * será renderizado uma lista contendo quais diretrizes foram violadas.
+ * 
+ * 
  * @param {TextboxProps} props 
- * @returns 
+ * @returns {React.JSX.Element} 
  */
 const Textbox = ({
     label,
@@ -68,7 +82,7 @@ const Textbox = ({
     return (
 
         <>
-            {errors.length == 0 &&
+            {errors.length === 0 &&
                 <div>
                     <label htmlFor={id}>
                         {isRequired ? label : <>{label}&nbsp;<small>(campo obrigatório)</small></>}
---
+++ src/lib/fields/textbox/Textbox.stories.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Textbox from "./Textbox";
+
+export default {
+    title: "Caixa de texto (Textbox)",
+    component: Textbox,
+    tags: ['autodocs'],
+}
+
+/**
+ * O componente é renderizado apenas quando estiver em conformidade com as diretrizes do eMAG,
+ * ou seja, quando todos os parâmetros forem passados corretamente.
+ */
+export const AccordingToGuidelines = () => (<Textbox
+    id="txtNome"
+    label="Nome"
+    placeholder="Digite o nome aqui..."
+    type="text"
+/>);
+
+AccordingToGuidelines.storyName = "Conformidade com as diretrizes do eMAG"
+
+/**
+ * Será listado todas as diretrizes violadas caso haja alguma inconformidade com as diretrizes do eMAG. 
+ */
+export const NonCompliace = {
+    component: Textbox
+};
+NonCompliace.storyName = "Inconformidade com as diretrizes do eMAG";
