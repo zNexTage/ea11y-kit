0aabf96111574d6b2405b1546011e51f5dadee37
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-04-22 18:14:17 -0300

    removido: demonstrado cor seleciona em um input
    o leitor de tela ficava lendo a cor em hexa, o que mais atrapalhava do que ajudava.

--- src/lib/fields/color/Color.jsx
+++ src/lib/fields/color/Color.jsx
@@ -1,28 +1,12 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef } from "react";
 import style from "./Color.module.css";
-import Textbox from "../textbox";
 
 const Color = ({
     id,
     name,
     label
 }) => {
-    const [selectedColor, setSelectedColor] = useState("");
     const colorRef = useRef();
-    const selectedColorRef = useRef();
-
-    /**
-     * 
-     * @param {InputEvent} event 
-     */
-    const onChange = event => {
-        setSelectedColor(event.target.value);
-        selectedColorRef.current.focus();
-    }
-
-    useEffect(() => {
-        setSelectedColor(colorRef.current.value);
-    }, []);
 
     return (
         <div className={style.ColorContainer}>
@@ -32,25 +16,8 @@ const Color = ({
                 </label>
                 <input
                     ref={colorRef}
-                    onChange={onChange}
                     className={style.Color} type="color" name={name} id={id} />
             </div>
-            <Textbox
-
-                id="txtSelectedColor"
-                label="Cor selecionada"
-                placeholder="Cor selecionada"
-                type="text"
-                aria-live="polite"
-                extraAttributes={{
-                    value: selectedColor,
-                    readOnly: true,
-                    ref: selectedColorRef
-                }}
-            />
-            {/* <span ref={selectedRef} id="selected-color" role="status" aria-live="polite" aria-label="Cor selecionada">
-                <b>Cor selecionada:</b> <span id="current-color"></span>
-            </span> */}
         </div>
     )
 }
