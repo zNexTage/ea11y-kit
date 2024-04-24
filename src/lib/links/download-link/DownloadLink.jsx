b4f2326fe0f4e3dd3cb73fbf9156a89c84070ffe
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-04-23 22:59:36 -0300

    DownloadLink: Criado componente.

---
+++ src/lib/links/download-link/DownloadLink.jsx
@@ -0,0 +1,113 @@
+import { useEffect, useRef, useState } from "react";
+import baseStyle from "../../Base.module.css";
+import GuidelineViolation from "../../../exceptions/GuidelineViolation/GuidelineViolation";
+import { DESCRIBE_LINKS_CLEARLY_AND_SUCCINCTLY } from "../../../utils/eMagGuidelineCode";
+import ComponentErrorList from "../../../components/component-error-list";
+import RequiredAttribute from "../../../exceptions/RequiredAttribute";
+
+/**
+ *  @typedef DownloadLinkProps
+ * 
+ * @property {string} href 
+ * @property {string} fileName
+ * @property {string} extension
+ * @property {string} size
+ * @property {string} unit
+ */
+
+/**
+ * Âncora para download de arquivos configurada com as diretrizes do eMAG
+ * 
+ * Diretrizes adotadas:
+ * 
+ * Atenção a diretriz
+ * Recomendação 1.9 – Não abrir novas instâncias sem a solicitação do usuário
+ * o eMAG não recomenda "O uso do atributo target=“_blank”;", pois "os usuários com deficiência visual podem 
+ * ter dificuldade em identificar que uma nova janela foi aberta. Além disso, estando em uma nova janela, 
+ * não conseguirão retornar à página anterior utilizando a opção voltar do navegador.". Entretanto, como o componente
+ * permitira visualizar o arquivo anexado, será utilizado o target _blank para que o arquivo seja aberto em uma nova guia.
+ * Entretanto, o eMAG descreve o seguinte para situações onde é necessário o uso do target _blank:
+ * "Quando for realmente necessária a abertura de um link em nova janela, é recomendado que tal ação seja 
+ * informada ao usuário no próprio texto do link. Isso permite ao usuário decidir se quer ou não sair da 
+ * janela ou aba em que se encontra e, caso decida acessar o link, ele saberá que se trata de uma nova aba 
+ * ou janela." Logo a frente do nome do arquivo e da extensão será acrescentado o texto (abre em nova janela).
+ * 
+ * Recomendação 2.1 - Disponibilizar todas as funções da página via teclado
+ * É possível interagir com o link utilizando o espaço do teclado.
+ * 
+ * Recomendação 3.5 – Descrever links clara e sucintamente
+ * - O componente utiliza como base a exigência: "Em links de arquivos para download, é necessário informar a extensão e o tamanho do arquivo no próprio texto do link" 
+ * 
+ * Recomendação 4.4 – Possibilitar que o elemento com foco seja visualmente evidente
+ * - Ao receber foco é aplicado uma borda vermelha de 2px do tipo solid no campo de texto.
+ * Além da borda, foi reforçado o destaque do componente através do atributo outline.
+ * 
+ * Propriedades
+ * - href: link para download do arquivo
+ * - fileName: Nome do arquivo
+ * - extension: Extensão do arquivo
+ * - size: tamanho do arquivo em bytes, kb, mb, etc...
+ * - unit: unidade do tamanho do arquivo bytes, kb, mb, etc...
+ * @param {DownloadLinkProps} props 
+ * @returns {React.JSX.Element}
+ */
+const DownloadLink = ({
+    href,
+    fileName,
+    extension,
+    size,
+    unit
+}) => {
+    const [violations, setViolations] = useState([]);
+
+    const downloadLinkRef = useRef();
+
+    useEffect(() => {
+        //Verifica se as propriedades informadas infligem alguma diretriz do eMAG.
+        const violationsAux = [];
+
+        if (!extension) {
+            violationsAux.push(new GuidelineViolation(DESCRIBE_LINKS_CLEARLY_AND_SUCCINCTLY, `Em links de arquivos para download, é necessário informar a extensão do arquivo.`))
+        }
+
+        if (!size) {
+            violationsAux.push(new GuidelineViolation(DESCRIBE_LINKS_CLEARLY_AND_SUCCINCTLY, `Em links de arquivos para download, é necessário informar o tamanho do arquivo.`))
+        }
+
+        if (!unit) {
+            violationsAux.push(new RequiredAttribute("É necessário informar a unidade do tamanho do arquivo (Kb, Mb, Gb, etc)."))
+        }
+
+        setViolations([...violationsAux]);
+    }, []);
+
+    /**
+     * Captura interação pelo teclado e permite interagir com o link usando a tecla espaço.
+     * @param {KeyboardEvent} event 
+     */
+    const onKeyDown = event => {
+        //TODO: Criar constantes para as teclas.
+        if (event.code == "Space") {
+            downloadLinkRef.current.click();
+        }
+    }
+
+    return (
+        <>
+            {
+                violations.length == 0 &&
+                <a
+                    ref={downloadLinkRef}
+                    target="_blank"
+                    onKeyDown={onKeyDown}
+                    className={baseStyle.Highlight}
+                    href={href}>
+                    {fileName} ({extension} {size}{unit}) - (abre em nova janela)
+                </a>
+            }
+            {violations.length > 0 && <ComponentErrorList errors={violations} />}
+        </>
+    )
+}
+
+export default DownloadLink;
---
+++ src/lib/links/download-link/DownloadLink.test.jsx
@@ -0,0 +1,106 @@
+import { render, screen, waitFor } from "@testing-library/react"
+import DownloadLink from "./DownloadLink"
+
+describe("[DownloadLink] - Violando diretriz 3.5", () => {
+    test("Deverá ser renderizado um alerta de violação da diretriz 3.5 ao omitir o atributo extesion", () => {
+        render(
+            <DownloadLink
+                fileName="teste"
+                href="http://teste.com"
+                size="10"
+                unit="KB"
+            />
+        )
+
+        const title = "Violação das diretrizes do eMAG";
+        const message = "Violação da diretriz 3.5 - Descrever links clara e sucintamente. Em links de arquivos para download, é necessário informar a extensão do arquivo. ";
+
+        const alert = screen.queryByRole("alert");
+
+        expect(alert).toBeInTheDocument();
+
+        expect(alert).toHaveTextContent(title);
+        expect(alert).toHaveTextContent(message);
+    })
+
+    test("Deverá ser renderizado um alerta de violação da diretriz 3.5 ao omitir o atributo size", () => {
+        render(
+            <DownloadLink
+                fileName="teste"
+                href="http://teste.com"
+                extension="JPG"
+                unit="KB"
+            />
+        )
+
+        const title = "Violação das diretrizes do eMAG";
+        const message = "Violação da diretriz 3.5 - Descrever links clara e sucintamente. Em links de arquivos para download, é necessário informar o tamanho do arquivo. ";
+
+        const alert = screen.queryByRole("alert");
+
+        expect(alert).toBeInTheDocument();
+
+        expect(alert).toHaveTextContent(title);
+        expect(alert).toHaveTextContent(message);
+    })
+});
+
+describe("[DownloadLink] - Conformidade com as diretrizes do eMAG", () => {
+    test("Diretriz 3.5 - Deverá ser renderizado o nome do arquivo, a extensão, o tamanho e unidade de medida", () => {
+        render(
+            <DownloadLink
+                fileName="teste"
+                href="http://teste.com"
+                size="10"
+                unit="KB"
+                extension=".jpg"
+            />
+        )
+
+        const link = screen.queryByRole("link");
+
+        expect(link).toHaveTextContent("teste (.jpg 10KB) - (abre em nova janela)")
+    });
+
+    test("Diretriz 4.4  - Deve adicionar uma borda e outline na cor vermelho ao focar no campo de texto", () => {
+        render(
+            <DownloadLink
+                fileName="teste"
+                href="http://teste.com"
+                size="10"
+                unit="KB"
+                extension=".jpg"
+            />
+        )
+
+        const link = screen.queryByRole("link");
+
+        link.focus();
+        expect(link).toHaveFocus();
+
+        waitFor(() => {
+            expect(link).toHaveStyle("border: 1px solid #F00;");
+            expect(link).toHaveStyle("outline:  2px solid #F00;")
+        });
+    })
+})
+
+describe("[DownloadLink] - Omitindo outras propriedades", () => {
+    test("Deverá ser renderizado um alerta ao omitir o atributo unit", () => {
+        render(
+            <DownloadLink
+                fileName="teste"
+                href="http://teste.com"
+                extension="JPG"
+
+            />
+        )
+
+        const message = "É necessário informar a unidade do tamanho do arquivo (Kb, Mb, Gb, etc).";
+
+        const alert = screen.queryByRole("alert");
+
+        expect(alert).toBeInTheDocument();
+        expect(alert).toHaveTextContent(message);
+    })
+})
