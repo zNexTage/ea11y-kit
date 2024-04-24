dc8652639a929b54fe7129f8187a60796a81ad3e
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-04-23 23:00:27 -0300

    Adicionado diretriz 3.5

--- src/utils/eMagGuidelineMessage.js
+++ src/utils/eMagGuidelineMessage.js
@@ -1,8 +1,9 @@
-import { ASSOCIATE_TAGS_WITH_YOUR_FIELDS, PROVIDE_INSTRUCTIONS_FOR_DATA_ENTRY } from "./eMagGuidelineCode";
+import { ASSOCIATE_TAGS_WITH_YOUR_FIELDS, PROVIDE_INSTRUCTIONS_FOR_DATA_ENTRY, DESCRIBE_LINKS_CLEARLY_AND_SUCCINCTLY } from "./eMagGuidelineCode";
 
 const guidelineMessages = new Map();
 
 guidelineMessages.set(ASSOCIATE_TAGS_WITH_YOUR_FIELDS, "Associar etiquetas aos seus campos");
 guidelineMessages.set(PROVIDE_INSTRUCTIONS_FOR_DATA_ENTRY, "Fornecer instruções para entrada de dados");
+guidelineMessages.set(DESCRIBE_LINKS_CLEARLY_AND_SUCCINCTLY, "Descrever links clara e sucintamente");
 
 export default guidelineMessages;
