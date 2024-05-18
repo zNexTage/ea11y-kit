fac4f00b93f31498fbd7ab3468db2dba475b9f2d
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-05-18 14:17:25 -0300

    Textarea: criado componente.

---
+++ src/lib/fields/textarea/Textarea.jsx
@@ -0,0 +1,88 @@
+
+import { useEffect, useState } from "react";
+import GuidelineViolation from "../../../exceptions/GuidelineViolation/GuidelineViolation";
+import { PROVIDE_INSTRUCTIONS_FOR_DATA_ENTRY } from "../../../utils/eMagGuidelineCode";
+import useFieldValidations from "../../hooks/validations/useFieldValidations";
+import baseStyle from "../BaseField.module.css";
+import ComponentErrorList from "../../../components/component-error-list";
+import RequiredAttribute from "../../../exceptions/RequiredAttribute";
+
+/**
+ * @typedef TextareaProps
+ * @property {string} id
+ * @property {string} name
+ * @property {string} label 
+ * @property {string} placeholder 
+ * @property {number} rows
+ * @property {number} cols
+ * @property {boolean} required
+ */
+
+/**
+ * Campo de texto com múltiplas linhas pré-configurado com as diretrizes do eMAG.
+ * 
+ * Recomendação 4.4 – Possibilitar que o elemento com foco seja visualmente evidente
+ * - Ao receber foco é aplicado uma borda vermelha de 2px do tipo solid no campo de texto.
+ * Além da borda, foi reforçado o destaque do componente através do atributo outline.
+ * 
+ * Recomendação 6.2 – Associar etiquetas aos seus campos: 
+ *  - O atributo id é obrigatório, e é utilizado para vincular a label ao campo de texto e identificar o input;
+ * 
+ * Recomendação 6.5 – Fornecer instruções para entrada de dados: 
+ *  - Para os campos obrigatórios é adicionado a informação *campo obrigatório* a frente da label para que
+ * leitores de telas possam comunicar ao usuário que o campo precisa ser preenchido;
+ * - O atributo *placeholder* é obrigatório para os campos e deverá apresentar uma dica de preenchimento do campo.
+ * A dica é importante para os leitores de tela. 
+ * @param {TextareaProps} props 
+ * @returns 
+ */
+const Textarea = ({
+    label, id, name, rows = 4, cols = 50, required = false, placeholder
+}) => {
+    const [errors, setErrors] = useState([]);
+
+    const violations = useFieldValidations(label, id);
+
+    useEffect(() => {
+        const textFieldViolations = [...violations];
+
+        if (!name) {
+            textFieldViolations.push(
+                new RequiredAttribute("É necessário especificar o nome (name) do campo. O atributo name é usado como referência quando os dados são enviados (https://www.w3schools.com/tags/att_name.asp).")
+            );
+        }
+
+        if (!placeholder) {
+            textFieldViolations.push(
+                new GuidelineViolation(PROVIDE_INSTRUCTIONS_FOR_DATA_ENTRY, `É necessário especificar uma dica para o campo de texto (placeholder). É importante informar uma dica, pois leitores de tela leem a dica e comunicam aos usuários.`)
+            );
+        }
+
+        setErrors([...textFieldViolations]);
+    }, [placeholder, violations]);
+
+    return (
+        <>
+            {errors.length == 0 &&
+                <div>
+                    <label htmlFor={id}>
+                        {label} {required && <small>(campo obrigatório)</small>}
+                    </label>
+                    <textarea
+                        required={required}
+                        rows={rows}
+                        cols={cols}
+                        className={baseStyle.field}
+                        name={name}
+                        placeholder={placeholder}
+                        id={id}>
+                    </textarea>
+                </div>
+            }
+
+            {errors.length > 0 && <ComponentErrorList errors={errors} />}
+        </>
+    )
+}
+
+export default Textarea;
---
+++ src/lib/fields/textarea/Textarea.test.jsx
@@ -0,0 +1,130 @@
+import { render, screen, waitFor, } from "@testing-library/react"
+import Textarea from "./Textarea";
+
+
+describe("[Textarea] - Violando diretriz 6.2 do eMAG", () => {
+    it("Deverá ser renderizado um alerta de violação da diretriz 6.2 ao omitir o atributo label", () => {
+        render(
+            <Textarea
+                id="ID"
+                placeholder="Teste"
+                name="teste"
+            />
+        );
+
+        const title = "Violação das diretrizes do eMAG";
+
+        const message = "Violação da diretriz 6.2 - Associar etiquetas aos seus campos";
+
+        const alert = screen.queryByRole("alert");
+        expect(alert).toBeInTheDocument();
+
+        expect(alert).toHaveTextContent(title);
+        expect(alert).toHaveTextContent(message);
+    });
+
+    it("Deverá ser renderizado um alerta de violação da diretriz 6.2 ao omitir o atributo id", () => {
+        render(
+            <Textarea
+                label="teste"
+                placeholder="Teste"
+                name="teste"
+            />
+        );
+
+        const title = "Violação das diretrizes do eMAG";
+
+        const message = "Violação da diretriz 6.2 - Associar etiquetas aos seus campos";
+
+        const alert = screen.queryByRole("alert");
+        expect(alert).toBeInTheDocument();
+
+        expect(alert).toHaveTextContent(title);
+        expect(alert).toHaveTextContent(message);
+    });
+});
+
+describe("[Textarea] - Violando diretriz 6.5 do eMAG", () => {
+    it("Deverá ser renderizado um alerta de violação da diretriz 6.5 ao omitir o atributo placeholder", () => {
+        render(
+            <Textarea
+                id="d"
+                label="label"
+                name="teste"
+            />
+        );
+
+        const title = "Violação das diretrizes do eMAG";
+
+        const message = "Violação da diretriz 6.5 - Fornecer instruções para entrada de dados";
+
+        const alert = screen.queryByRole("alert");
+        expect(alert).toBeInTheDocument();
+
+        expect(alert).toHaveTextContent(title);
+        expect(alert).toHaveTextContent(message);
+    })
+});
+
+describe("[Textarea] - Omitindo outros atributos", () => {
+    it("Deverá ser renderizado um alerta ao omitir o atributo name", () => {
+        render(
+            <Textarea
+                id="d"
+                label="label"
+                placeholder="label"
+                required
+            />
+        );
+
+        const title = "Violação das diretrizes do eMAG";
+
+        const message = "É necessário especificar o nome (name) do campo. O atributo name é usado como referência quando os dados são enviados (https://www.w3schools.com/tags/att_name.asp).";
+
+        const alert = screen.queryByRole("alert");
+        expect(alert).toBeInTheDocument();
+
+        expect(alert).toHaveTextContent(title);
+        expect(alert).toHaveTextContent(message);
+    });
+});
+
+describe("[Textarea] - Conformidade com as diretrizes do eMAG", () => {
+    it("Deve aparecer a indicação (campo obrigatório) quando isRequired for igual a true", () => {
+        render(
+            <Textarea
+                id="txtDescricao"
+                label="Descrição"
+                placeholder="Digite aqui a descrição do produto"
+                required
+                name="descricao"
+            />
+        )
+
+        const field = screen.getByLabelText("Descrição (campo obrigatório)");
+
+        waitFor(() => expect(field.parentElement).toHaveTextContent("(campo obrigatório)"));
+
+    });
+
+    it("Deve adicionar uma borda e outline na cor vermelho ao focar no campo de texto", () => {
+        render(
+            <Textarea
+                id="txtDescricao"
+                label="Descrição"
+                placeholder="Digite aqui a descrição do produto"
+                required
+                name="descricao"
+            />
+        );
+
+        const field = screen.getByLabelText("Descrição (campo obrigatório)");
+        field.focus();
+        expect(field).toHaveFocus();
+
+        waitFor(() => {
+            expect(field).toHaveStyle("border: 1px solid #F00;");
+            expect(field).toHaveStyle("outline:  2px solid #F00;")
+        });
+    })
+});
