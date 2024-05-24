175edda1ca0977abf782c3a2929d2fe43913f7f7
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-05-24 13:15:51 -0300

    Alterado padding dos campos.

--- src/lib/fields/shared-styles/Field.style.js
+++ src/lib/fields/shared-styles/Field.style.js
@@ -2,7 +2,7 @@ import baseTheme from "../../../stitches.config";
 
 const fieldCss = baseTheme.css({
     display: 'block',
-    padding: '5px',
+    padding: '.55em',
     borderRadius: '2px',
     border: '2px solid $colors$borderColor',
     width: '100%',
