debe5c9dc230967b7bf0eef76fd4230479629614
christian <christianoliveira1908@hotmail.com>
2024-05-28 16:22:17 -0300

    Adicionado código e mensagem da violação da diretriz de imagem.

--- src/utils/eMagGuidelineMessage.js
+++ src/utils/eMagGuidelineMessage.js
@@ -1,9 +1,15 @@
-import { ASSOCIATE_TAGS_WITH_YOUR_FIELDS, PROVIDE_INSTRUCTIONS_FOR_DATA_ENTRY, DESCRIBE_LINKS_CLEARLY_AND_SUCCINCTLY } from "./eMagGuidelineCode";
+import {
+    ASSOCIATE_TAGS_WITH_YOUR_FIELDS,
+    PROVIDE_INSTRUCTIONS_FOR_DATA_ENTRY,
+    DESCRIBE_LINKS_CLEARLY_AND_SUCCINCTLY,
+    PROVIDE_TEXT_ALTERNATIVE_TO_WEBSITE_IMAGES
+} from "./eMagGuidelineCode";
 
 const guidelineMessages = new Map();
 
 guidelineMessages.set(ASSOCIATE_TAGS_WITH_YOUR_FIELDS, "Associar etiquetas aos seus campos");
 guidelineMessages.set(PROVIDE_INSTRUCTIONS_FOR_DATA_ENTRY, "Fornecer instruções para entrada de dados");
 guidelineMessages.set(DESCRIBE_LINKS_CLEARLY_AND_SUCCINCTLY, "Descrever links clara e sucintamente");
+guidelineMessages.set(PROVIDE_TEXT_ALTERNATIVE_TO_WEBSITE_IMAGES, "Fornecer alternativa em texto para as imagens do sítio");
 
 export default guidelineMessages;
