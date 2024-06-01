5ee928e09507e66da44259292be39bdc3aa08df7
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-06-01 14:15:16 -0300

    adicionado diretrizes relacionadas a tabela.

--- src/utils/eMagGuidelineMessage.js
+++ src/utils/eMagGuidelineMessage.js
@@ -3,7 +3,8 @@ import {
     PROVIDE_INSTRUCTIONS_FOR_DATA_ENTRY,
     DESCRIBE_LINKS_CLEARLY_AND_SUCCINCTLY,
     PROVIDE_TEXT_ALTERNATIVE_TO_WEBSITE_IMAGES,
-    PROVIDE_ALTERNATIVE_TO_AUDIO
+    PROVIDE_ALTERNATIVE_TO_AUDIO,
+    IN_TABLES_USES_TITLE_AND_SUMMARIES_APPROPRIATELY
 } from "./eMagGuidelineCode";
 
 const guidelineMessages = new Map();
@@ -11,6 +12,7 @@ const guidelineMessages = new Map();
 guidelineMessages.set(ASSOCIATE_TAGS_WITH_YOUR_FIELDS, "Associar etiquetas aos seus campos");
 guidelineMessages.set(PROVIDE_INSTRUCTIONS_FOR_DATA_ENTRY, "Fornecer instruções para entrada de dados");
 guidelineMessages.set(DESCRIBE_LINKS_CLEARLY_AND_SUCCINCTLY, "Descrever links clara e sucintamente");
+guidelineMessages.set(IN_TABLES_USES_TITLE_AND_SUMMARIES_APPROPRIATELY, "Em tabelas, utilizar títulos e resumos de forma apropriada");
 guidelineMessages.set(PROVIDE_ALTERNATIVE_TO_AUDIO, "Fornecer alternativa para áudio");
 guidelineMessages.set(PROVIDE_TEXT_ALTERNATIVE_TO_WEBSITE_IMAGES, "Fornecer alternativa em texto para as imagens do sítio");
 
