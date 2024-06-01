1c6daf93799d4b0948b4a86f9eaec8a08e38d422
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-05-31 21:58:55 -0300

    Link: adotado uso de css-in-js.

--- src/lib/links/link/Link.jsx
+++ src/lib/links/link/Link.jsx
@@ -1,3 +1,4 @@
+import { styled } from "@stitches/react";
 import { lightTheme } from "../../../stitches.config";
 import { fieldHightlight } from "../../fields/shared-styles/Field.style";
 
@@ -6,11 +7,14 @@ const TARGET_BLANK = "_blank";
 /**
  * @typedef LinkProps 
  * @property {React.ReactNode} children
+ * @property {import("@stitches/react").CSS} css
  * */
 
 /**
  * @typedef {LinkProps & React.HTMLProps<HTMLAnchorElement>} ExtendedLinkProps
  */
+
+const LinkStyled = styled("a", {});
 
 /**
  * Link pré-configurado com as diretrizes do eMAG. A descrição do link deve ser clara e deve expressar
@@ -33,30 +37,36 @@ const TARGET_BLANK = "_blank";
  * @param {ExtendedLinkProps} props 
  * @returns 
  */
-const Link = ({ children, href, target, ...rest }) => {
+const Link = ({ children, css, href, target, ...rest }) => {
 
-    const extraProps = {};
+  const extraProps = {};
 
-    const isTargetBlank = target === TARGET_BLANK;
+  const isTargetBlank = target === TARGET_BLANK;
 
-    /*
-    adiciona a configuração noopener e noreferrer.
-    noopener:  
-      - Previne que a nova página (a que foi aberta com target="_blank") tenha acesso ao objeto window.opener.
-      - Sem essa precaução, a nova página poderia executar scripts que manipulassem a página original, o que pode ser um risco de segurança.
-    noreferrer:
-      - Impede que a página de destino receba informações sobre a origem (referer) do link.
-      - Isso significa que a nova página não saberá de qual página você veio, preservando a privacidade do usuário e evitando que a nova página saiba de onde o tráfego está vindo.
-    */
-    if (isTargetBlank) {
-        extraProps["rel"] = "noopener noreferrer"
-    }
+  /*
+  adiciona a configuração noopener e noreferrer.
+  noopener:  
+    - Previne que a nova página (a que foi aberta com target="_blank") tenha acesso ao objeto window.opener.
+    - Sem essa precaução, a nova página poderia executar scripts que manipulassem a página original, o que pode ser um risco de segurança.
+  noreferrer:
+    - Impede que a página de destino receba informações sobre a origem (referer) do link.
+    - Isso significa que a nova página não saberá de qual página você veio, preservando a privacidade do usuário e evitando que a nova página saiba de onde o tráfego está vindo.
+  */
+  if (isTargetBlank) {
+    extraProps["rel"] = "noopener noreferrer"
+  }
 
-    return (
-        <a {...rest} target={target} href={href} className={`${lightTheme} ${fieldHightlight}`} {...extraProps}>
-            {children} {isTargetBlank && <span>(abre em nova guia)</span>}
-        </a>
-    )
+  return (
+    <LinkStyled
+      {...rest}
+      target={target}
+      href={href}
+      className={`${lightTheme} ${fieldHightlight}`} {...extraProps}
+      css={css}
+    >
+      {children} {isTargetBlank && <span>(abre em nova guia)</span>}
+    </LinkStyled>
+  )
 }
 
 export default Link;
