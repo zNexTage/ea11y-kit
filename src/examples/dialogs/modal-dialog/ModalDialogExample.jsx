5ae38f40fb8bc40a2dc5918c42f4bfde527ff8e3
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-06-24 22:04:00 -0300

    Dialog: exemplo de uso do Dialog.

---
+++ src/examples/dialogs/modal-dialog/ModalDialogExample.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { Dialog } from "../../../lib/dialogs/modal-dialog";
+import Button from "../../../lib/fields/button/Button";
+
+const ModalDialogExample = () => {
+    const [show, setShow] = useState(true);
+
+    return (
+        <>
+            <Button onClick={() => setShow(true)}>
+                Abrir modal
+            </Button>
+            <Dialog.Root
+                show={show}
+                type="modal"
+                body={(
+                    <Dialog.Body>
+                        <p>
+                            Esse é o corpo do modal
+                        </p>
+                    </Dialog.Body>
+                )}
+                header={(
+                    <Dialog.Header
+                        onCloseClick={() => setShow(false)}
+                        title={"Teste"}>
+                        <p>
+                            Testando dialog
+                        </p>
+                    </Dialog.Header>
+                )}
+            />
+        </>
+    )
+}
+
+export default ModalDialogExample;
