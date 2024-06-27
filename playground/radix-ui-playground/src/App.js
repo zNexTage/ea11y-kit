698067a0cb6eb2a0455149300c33f18982aeb0f6
christian <christianoliveira1908@hotmail.com>
2024-06-27 16:58:59 -0300

    Radix UI: add biblioteca de ícones e exemplo com o Select.

--- playground/radix-ui-playground/src/App.js
+++ playground/radix-ui-playground/src/App.js
@@ -1,10 +1,17 @@
 import AvatarExample from "./examples/AvatarExample";
+import CheckboxExample from "./examples/CheckboxExample";
+import FormExample from "./examples/FormExample";
+import RadioGroupExample from "./examples/RadioGroupExample";
+import SelectExample from "./examples/SelectExample";
 
 
 function App() {
   return (
     <div>
-      <AvatarExample />
+      {/* <CheckboxExample /> */}
+      {/* <FormExample /> */}
+      {/* <RadioGroupExample /> */}
+      <SelectExample />
     </div>
   );
 }
