5c0959036b90956236fbb947af66980dbc2ae71d
christian <christianoliveira1908@hotmail.com>
2024-07-11 15:46:59 -0300

    Material: utilizando Link

--- playground/material-ui-playground/src/App.js
+++ playground/material-ui-playground/src/App.js
@@ -1,12 +1,15 @@
 import AvatarExample from "./examples/AvatarExample";
 import FormExample from "./examples/FormExample";
+import LinkExample from "./examples/LinkExample";
 
 function App() {
   return (
     <div className="App">
       {/* <AvatarExample /> */}
 
-      <FormExample />
+      {/* <FormExample /> */}
+
+      <LinkExample />
     </div>
   );
 }
---
+++ playground/material-ui-playground/src/examples/LinkExample.jsx
@@ -0,0 +1,10 @@
+import Link from '@mui/material/Link';
+
+
+const LinkExample = () => (
+    <Link href="https://acervo.ceweb.br/acervos/conteudo/8d6506be-5d3f-4e88-900f-df8749d81c36">
+        Cartilha de acessibilidade do W3C
+    </Link>
+)
+
+export default LinkExample;
