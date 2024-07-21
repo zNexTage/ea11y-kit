f688b20c5bc1984791443f71bd1c1162780cf324
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-07-20 23:00:07 -0300

    Textbox: melhorado mensagem de violação de diretriz.

--- src/lib/hooks/validations/useFieldValidations.jsx
+++ src/lib/hooks/validations/useFieldValidations.jsx
@@ -22,12 +22,12 @@ const useFieldValidations = (label, id) => {
 
         if (!label) {
             // Violou a diretriz 6.2. Não informou a etiqueta (label).
-            errorsAux.push(new GuidelineViolation(ASSOCIATE_TAGS_WITH_YOUR_FIELDS, "É essencial que todos os campos de entrada, como caixas de texto, caixas de seleção (checkbox), botões de rádio (radio button), entre outros, tenham uma etiqueta (label) associada. A etiqueta fornece uma descrição clara do propósito do campo, o que é fundamental para orientar todos os usuários, especialmente aqueles com dificuldades visuais que dependem de leitores de tela. Quando os usuários focam em um campo de entrada, o leitor de tela deve ler a etiqueta para fornecer a devida orientação."));           
+            errorsAux.push(new GuidelineViolation(ASSOCIATE_TAGS_WITH_YOUR_FIELDS, "É crucial que todos os campos de entrada, como caixas de texto, caixas de seleção (checkbox), botões de rádio (radio button), entre outros, tenham uma etiqueta (label) associada. A etiqueta fornece uma descrição clara do propósito do campo, o que é essencial para guiar todos os usuários, especialmente aqueles com dificuldades visuais que dependem de leitores de tela. Quando os usuários focam em um campo de entrada, o leitor de tela deve ler a etiqueta para fornecer a orientação adequada."));           
         }
 
         if (!id) {
             // Violou a diretriz 6.2. Não informou o id.
-            errorsAux.push(new GuidelineViolation(ASSOCIATE_TAGS_WITH_YOUR_FIELDS, "Para associar corretamente uma etiqueta (label) a um campo de texto e contextualizá-lo, é fundamental fornecer a propriedade 'id' correspondente"));
+            errorsAux.push(new GuidelineViolation(ASSOCIATE_TAGS_WITH_YOUR_FIELDS, "Para associar corretamente uma etiqueta (label) a um campo de texto e contextualizá-lo, é fundamental fornecer a propriedade 'id' correspondente."));
         }
 
         setErrors(errorsAux);
