74929fad3ba88e505710629c2a73c141ff91286b
christian <christianoliveira1908@hotmail.com>
2024-07-31 11:08:07 -0300

    Material UI: ajustado form para teste de contraste.

--- playground/material-ui-playground/src/examples/FormExample.jsx
+++ playground/material-ui-playground/src/examples/FormExample.jsx
@@ -1,74 +1,169 @@
-import { Button, Checkbox, FormControl, FormControlLabel, FormGroup, FormHelperText, FormLabel, InputLabel, MenuItem, Radio, RadioGroup, Select, Slider, Switch, TextField } from "@mui/material";
+// import { Button, Checkbox, FormControl, FormControlLabel, FormGroup, FormHelperText, FormLabel, InputLabel, MenuItem, Radio, RadioGroup, Select, Slider, Switch, TextField } from "@mui/material";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 const FormExample = () => {
     return (
-        <form>
-            <FormControl sx={{ m: 3 }} component="fieldset" variant="standard">
-                <FormLabel component="legend">Assign responsibility</FormLabel>
-                <FormGroup>
-                    <FormControlLabel
-                        control={
-                            <Checkbox name="gilad" />
-                        }
-                        label="Gilad Gray"
-                    />
-                    <FormControlLabel
-                        control={
-                            <Checkbox name="jason" />
-                        }
-                        label="Jason Killian"
-                    />
-                    <FormControlLabel
-                        control={
-                            <Checkbox name="antoine" />
-                        }
-                        label="Antoine Llorca"
-                    />
-                </FormGroup>
-                <FormHelperText>Be careful</FormHelperText>
-            </FormControl>
-
-            <br />
-            <FormControl>
-                <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
-                <RadioGroup
-                    name="radio-buttons-group"
-                >
-                    <FormControlLabel value="female" control={<Radio />} label="Female" />
-                    <FormControlLabel value="male" control={<Radio />} label="Male" />
-                    <FormControlLabel value="other" control={<Radio />} label="Other" />
-                </RadioGroup>
-            </FormControl>
-
-            <br />
-            <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">Age</InputLabel>
-                <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    label="Age"
-                >
-                    <MenuItem value={10}>Ten</MenuItem>
-                    <MenuItem value={20}>Twenty</MenuItem>
-                    <MenuItem value={30}>Thirty</MenuItem>
-                </Select>
-            </FormControl>
-
-            <Slider defaultValue={30} aria-label="Volume" />
-
-            <FormGroup>
-                <FormControlLabel control={<Switch defaultChecked />} label="Label" />
-                <FormControlLabel required control={<Switch />} label="Required" />
-                <FormControlLabel disabled control={<Switch />} label="Disabled" />
-            </FormGroup>
-
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-            <TextField id="filled-basic" label="Filled" variant="filled" />
-            <TextField id="standard-basic" label="Standard" variant="standard" />
-
-            <br />
-            <Button onClick={event => console.log("ALO MUNDO")} variant="contained">Enviar</Button>
-        </form>
+        <Box
+            component="form"
+            sx={{
+                '& .MuiTextField-root': { m: 1, width: '25ch' },
+            }}
+            noValidate
+            autoComplete="off"
+        >
+            <div>
+                <TextField
+                    required
+                    id="outlined-required"
+                    label="Required"
+                    defaultValue="Hello World"
+                />
+                <TextField
+                    disabled
+                    id="outlined-disabled"
+                    label="Disabled"
+                    defaultValue="Hello World"
+                />
+                <TextField
+                    id="outlined-password-input"
+                    label="Password"
+                    type="password"
+                    autoComplete="current-password"
+                />
+                <TextField
+                    id="outlined-read-only-input"
+                    label="Read Only"
+                    defaultValue="Hello World"
+                    InputProps={{
+                        readOnly: true,
+                    }}
+                />
+                <TextField
+                    id="outlined-number"
+                    label="Number"
+                    type="number"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                />
+                <TextField id="outlined-search" label="Search field" type="search" />
+                <TextField
+                    id="outlined-helperText"
+                    label="Helper text"
+                    defaultValue="Default Value"
+                    helperText="Some important text"
+                />
+            </div>
+            <div>
+                <TextField
+                    required
+                    id="filled-required"
+                    label="Required"
+                    defaultValue="Hello World"
+                    variant="filled"
+                />
+                <TextField
+                    disabled
+                    id="filled-disabled"
+                    label="Disabled"
+                    defaultValue="Hello World"
+                    variant="filled"
+                />
+                <TextField
+                    id="filled-password-input"
+                    label="Password"
+                    type="password"
+                    autoComplete="current-password"
+                    variant="filled"
+                />
+                <TextField
+                    id="filled-read-only-input"
+                    label="Read Only"
+                    defaultValue="Hello World"
+                    InputProps={{
+                        readOnly: true,
+                    }}
+                    variant="filled"
+                />
+                <TextField
+                    id="filled-number"
+                    label="Number"
+                    type="number"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    variant="filled"
+                />
+                <TextField
+                    id="filled-search"
+                    label="Search field"
+                    type="search"
+                    variant="filled"
+                />
+                <TextField
+                    id="filled-helperText"
+                    label="Helper text"
+                    defaultValue="Default Value"
+                    helperText="Some important text"
+                    variant="filled"
+                />
+            </div>
+            <div>
+                <TextField
+                    required
+                    id="standard-required"
+                    label="Required"
+                    defaultValue="Hello World"
+                    variant="standard"
+                />
+                <TextField
+                    disabled
+                    id="standard-disabled"
+                    label="Disabled"
+                    defaultValue="Hello World"
+                    variant="standard"
+                />
+                <TextField
+                    id="standard-password-input"
+                    label="Password"
+                    type="password"
+                    autoComplete="current-password"
+                    variant="standard"
+                />
+                <TextField
+                    id="standard-read-only-input"
+                    label="Read Only"
+                    defaultValue="Hello World"
+                    InputProps={{
+                        readOnly: true,
+                    }}
+                    variant="standard"
+                />
+                <TextField
+                    id="standard-number"
+                    label="Number"
+                    type="number"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    variant="standard"
+                />
+                <TextField
+                    id="standard-search"
+                    label="Search field"
+                    type="search"
+                    variant="standard"
+                />
+                <TextField
+                    id="standard-helperText"
+                    label="Helper text"
+                    defaultValue="Default Value"
+                    helperText="Some important text"
+                    variant="standard"
+                />
+            </div>
+        </Box>
     )
 }
 
