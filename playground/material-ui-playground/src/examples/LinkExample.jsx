e0b5311d6b2b18717ab64051a7a34f5b9409c425
christian <christianoliveira1908@hotmail.com>
2024-08-03 15:42:28 -0300

    Material UI: exemplos com links.

--- playground/material-ui-playground/src/examples/LinkExample.jsx
+++ playground/material-ui-playground/src/examples/LinkExample.jsx
@@ -2,9 +2,19 @@ import Link from '@mui/material/Link';
 
 
 const LinkExample = () => (
-    <Link href="https://acervo.ceweb.br/acervos/conteudo/8d6506be-5d3f-4e88-900f-df8749d81c36">
-        Cartilha de acessibilidade do W3C
-    </Link>
+    <>
+        <Link href="#">Link</Link>
+        <br />
+        <br />
+        <Link href="#" color="inherit">
+            {'color="inherit"'}
+        </Link>
+        <br />
+        <br />
+        <Link href="#" variant="body2">
+            {'variant="body2"'}
+        </Link>
+    </>
 )
 
 export default LinkExample;
