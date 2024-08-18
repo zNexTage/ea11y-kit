f03d92c82ce6381c0355e82fac1e3d12e14aa4d2
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-08-18 15:25:16 -0300

    permitido axe-core no modo produção.

--- src/index.js
+++ src/index.js
@@ -17,7 +17,5 @@ root.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-if (process.env.NODE_ENV !== 'production') {
-  const axe = require('@axe-core/react');
-  axe(React, ReactDOM, 1000);
-}
+const axe = require('@axe-core/react');
+axe(React, ReactDOM, 1000);
